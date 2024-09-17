c337b68ac0ac5b203d63b8975176c172b67f4770
Laput0 <1737261850wang@gmail.com>
2024-09-17 05:47:42 +0000

    update mosdns

--- debian_sysroot/etc/mosdns/update_rules.sh
+++ debian_sysroot/etc/mosdns/update_rules.sh
@@ -3,9 +3,12 @@ test -d $rules_dir || mkdir $rules_dir
 cd $rules_dir
 
 # geosite
-curl -sS -o geosite_private.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/private.txt
-curl -sS -o geosite_cn.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/cn.txt
+#curl -sS -o geosite_private.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/private.txt
+#curl -sS -o geosite_cn.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/cn.txt
+
+curl -sS -o reject-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt
+curl -sS -o direct-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt 
 
 # geoip
-curl -sS -o geoip_private.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/private.txt
-curl -sS -o geoip_cn.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt
+#curl -sS -o geoip_private.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/private.txt
+#curl -sS -o geoip_cn.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt
