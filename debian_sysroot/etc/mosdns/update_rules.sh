4322b4255561258ae2b4be78279f4ddc79e097d3
Laputa0 <1737261850wang@gmail.com>
2025-07-20 20:01:24 +0800

    update

--- debian_sysroot/etc/mosdns/update_rules.sh
+++ debian_sysroot/etc/mosdns/update_rules.sh
@@ -6,8 +6,9 @@ cd $rules_dir
 #curl -sS -o geosite_private.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/private.txt
 #curl -sS -o geosite_cn.txt https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/cn.txt
 
-curl -sS -o reject-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt
-curl -sS -o direct-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt 
+curl -sS -o reject-list.txt https://raw.githubusercontent.com/pmkol/easymosdns/refs/heads/rules/ad_domain_list.txt
+curl -sS -o direct-list.txt https://raw.githubusercontent.com/pmkol/easymosdns/refs/heads/rules/china_domain_list.txt
+curl -sS -o cdn-list.txt https://raw.githubusercontent.com/pmkol/easymosdns/refs/heads/rules/cdn_domain_list.txt
 
 # geoip
 #curl -sS -o geoip_private.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/private.txt
